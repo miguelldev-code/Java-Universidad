1f54df79e20dd06a1068c94e5307d2cc90094d97
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-04-04 02:29:20 -0500

    explicacion varibles en java

--- src/datos/Variales.java
+++
@@ -1,8 +0,0 @@
-package datos;
-
-public class Variales {
-    public static void main(String[] args) {
-
-    }
-
-}
---
+++ src/datos/variables.java
@@ -0,0 +1,43 @@
+package datos;
+
+public class variables {
+    public static void main(String[] args) {
+
+        // VARIABLES EN JAVA
+
+        // Declaración e inicialización de variables
+        String miVariable = "Hola desde Java";
+        System.out.println(miVariable);
+
+        // Ejemplos de asignaciones
+        String nombre = "Juan";
+        int edad = 20;
+        double altura = 1.75;
+        boolean esEstudiante = true;
+
+        System.out.println(nombre); // Imprime: Juan
+        System.out.println(edad); // Imprime: 20
+        System.out.println(altura); // Imprime: 1.75
+        System.out.println(esEstudiante); // Imprime: true
+
+        // Operaciones con variables
+        int x = 10;
+        int y = 12;
+        int z = x + y;
+        System.out.println(z); // Imprime: 22
+
+        // Concatenación entre texto y variables
+        String usuario = "User1";
+        String mensajeBienvenida = "Hola, " + usuario;
+        System.out.println(mensajeBienvenida); // Imprime: Hola, User1
+
+        // Distinción entre mayúsculas y minúsculas
+        String variable1 = "Hola";
+        String Variable1 = "Adiós"; // Diferente por la mayúscula inicial
+
+        System.out.println(variable1); // Imprime: Hola
+        System.out.println(Variable1); // Imprime: Adiós
+
+    }
+
+}
