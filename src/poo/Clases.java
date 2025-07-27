8176e898c57f29cae4dc908cda287e241788801c
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-07-26 23:49:39 -0500

    POO - Clases

---
+++ src/poo/Clases.java
@@ -0,0 +1,17 @@
+package poo;
+
+// Plantilla o molde que permite construir objetos
+// Representan entidades del mundo real
+// EJ: Auto, Producto, Estudiantes...
+// Poseen atributos y metodos  (Caracteristicas y Acciones)
+// No puedens ser utilizadas directamente (sino mediante instancionas de objetos)
+
+// Modificador de acceso - Identifica una clase - Nombre de la clase
+public class Clases {
+
+    // Atributos
+    int id;
+    String nombre;
+    String apellido;
+
+}
