d932fd37c0d432fcfc155e0556eeed6112c05a24
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-07-27 22:21:38 -0500

    POO - Objetos

--- src/poo/Objetos.java
+++ src/poo/Objetos.java
@@ -1,4 +1,15 @@
 package poo;
 
 public class Objetos {
+    public static void main(String[] args) {
+
+        // Sin valor
+        Constructores c1 = new Constructores();
+
+        // Con valor/es
+        Constructores c2 = new Constructores(1, "miguel", "ortiz");
+
+
+
+    }
 }
