b705774c89f4604d54cbe60e5d83d7e597febb99
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2025-07-28 23:32:33 -0500

    POO - Get y Set

---
+++ src/poo/GetSet.java
@@ -0,0 +1,7 @@
+package poo;
+
+// Get y Set son utilizados para acceder y modificar los valores de los atributos privados de una clase
+
+// Permite un control mas precios sobre como ser manipulan los datos de un objeto
+public class GetSet {
+}
